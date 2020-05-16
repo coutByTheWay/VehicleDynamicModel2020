8ad6814aa3cfe194f1a5734f7bc469485c5541ac
coutByTheWay <helloworld55@ymail.com>
2020-05-16 12:56:44 -0500

    removed slprj files from repo. These files are used by the compiler, and will be different for each computer. We do not need to track them.

--- slprj/sim/Traction_Model/Traction_Model_capi.h
+++
@@ -1,6 +0,0 @@
-#ifndef RTW_HEADER_Traction_Model_capi_h_
-#define RTW_HEADER_Traction_Model_capi_h_
-#include "Traction_Model.h"
-extern void Traction_Model_InitializeDataMapInfo ( dodztnquqb * const
-iv3y41y4ea , mk15kpcwx1 * localX , void * sysRanPtr , int contextTid ) ;
-#endif
